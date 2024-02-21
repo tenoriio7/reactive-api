149d026805ae62d3c0b989eb96aa0941fcb54843
Vinicius Tenório <vinicius.tenorio02@gmail.com>
2024-02-21 15:25:55 -0300

    corrections

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation ("com.google.code.gson:gson:2.8.8")
