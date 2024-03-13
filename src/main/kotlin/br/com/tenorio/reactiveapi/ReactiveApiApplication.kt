5f9dcd3fb348da649a7dc38285484dc7b740686c
Vinicius Tenório <vinicius.tenorio02@gmail.com>
2024-03-13 15:39:30 -0300

    chaos

--- src/main/kotlin/br/com/tenorio/reactiveapi/ReactiveApiApplication.kt
+++ src/main/kotlin/br/com/tenorio/reactiveapi/ReactiveApiApplication.kt
@@ -1,7 +1,6 @@
 package br.com.tenorio.reactiveapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
