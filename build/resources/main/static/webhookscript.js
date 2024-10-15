c3057b2f2a0e749db1c96bbedf617904fd9e0c0c
uolvlopes <65618650+uolvlopes@users.noreply.github.com>
2024-10-15 14:28:44 -0300

    corrections

--- build/resources/main/static/webhookscript.js
+++ build/resources/main/static/webhookscript.js
@@ -1,15 +1,20 @@
+// instancia de conexao com websocket
 var ws = new WebSocket("ws://localhost:8080/webhooks")
+
+//elemento que contem o webhook
 var webhook = document.getElementById("webhook")
 
+// quando alguma mensagem via websocket chega
 ws.onmessage = function (event) {
-    var mensagem = event.data
-    var div = document.createElement("div")
-    div.classList.add("message")
-    div.textContent = JSON.stringify(JSON.parse(mensagem), null, 2)
-    div.style.transform = "scale(0)"
-    webhook.appendChild(div)
+
+    var mesage = event.data // extrai o valor
+    var div = document.createElement("div") // cria uma div pra colocar a msg
+    div.classList.add("message") //adiciona
+    div.textContent = JSON.stringify(JSON.parse(mesage), null, 2) // parse para json
+    div.style.transform = "scale(0)" // transforma o style
+    webhook.appendChild(div) // coloca o valor
     setTimeout(function () {
         div.style.transform = "scale(1)"
     }, 0)
-    webhook.scrollTop = webhook.scrollHeight
+    webhook.scrollTop = webhook.scrollHeight // configura o scroll para ficar alinhado
 }
